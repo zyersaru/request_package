9ef4d8a32bc208be4ad8c79cd157947741d86cd1
Edmond <edmondcheung604@hotmail.com>
2017-07-04 03:51:12 +0000

    Fixed where the download complete line goes

--- request_test.js
+++ request_test.js
@@ -2,14 +2,16 @@ var request = require('request');
 var fs = require('fs');
 
 request.get('https://sytantris.github.io/http-examples/future.jpg')
-       .on('error', function (err) {
-          console.log('Error has occurred.', response.statusCode);
-        })
-       .on('response', function (response) {
-          console.log('Response Status Code: ', response.statusCode);
-          console.log('Response Status Message: ', response.statusMessage);
-          console.log('Downloading image...');
-          console.log('Response Header: ', response.headers['content-type']);
-          console.log('Download complete.');
-       })
-       .pipe(fs.createWriteStream('./future.jpg'));
+  .on('error', function (err) {
+    console.log('Error has occurred.', response.statusCode);
+  })
+  .on('response', function (response) {
+    console.log('Response Status Code: ', response.statusCode);
+    console.log('Response Status Message: ', response.statusMessage);
+    console.log('Response Header: ', response.headers['content-type']);
+    console.log('Downloading image...');
+  })
+  .pipe(fs.createWriteStream('./future.jpg'))
+  .on('finish', function () {
+    console.log('Download complete.')
+  });
